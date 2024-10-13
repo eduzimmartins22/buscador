6584ef771e15135feeaf2a3babd9a0b1e1814288
eduzimmartins22 <dudukofduduzinho@gmail.com>
2024-10-13 11:26:40 -0300

    DadosCEP

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import  './style.css';
+import api from './services/api'
+
 
 function App() {
+
+const [input, setInput] = useState("")
+const [cep , setCep] = useState({})
+
+async function handeleSearch(){
+  if(input == "")
+  {
+    alert("Digite algum CEP")
+    return; 
+  }
+  try{
+const resposta = await api.get(`${input}/json`)
+  setCep(resposta.data)
+  setInput("")
+  }
+  catch{
+    alert("ERRO AO BUSCAR O CEP")
+    setInput ('')
+
+  }
+}
+
+
+
   return (
     <div className="container">
-     <h1>JOGO DO CEP</h1>
+     <h1 className="title">Dados do CEP</h1>
 
-    <div className = "containerInput">
+      <div className = "containerInput">
       
-       <input type = "text" placeholder="Digite o Cep que quer descobrir"></input>
-      <button className="ButtonProcurar">
-         <FiSearch size={20} color ="#fff"/> 
+        <input type = "text" placeholder="Digite o CEP" value = {input} onChange={(e => setInput(e.target.value))} />
 
-      </button>
+        <button className="ButtonProcurar" onClick={handeleSearch}>
+          <FiSearch size={20} color ="#fff"/> 
+        </button>
 
-    </div>
-      <main  className="main">
-      <h2>CEP : 29181973</h2>
-      <span>Rua bla bla bla</span>
-      <br></br>
-      <span>Ponto de referência</span>
-      <br></br>
-      <span>avenida Rosa</span>
-      <br></br>
-      <span>Numer tal tal tal</span>
+      </div>
+      <div className="conteudo">
+      {Object.keys(cep).length > 0 && 
+      (
+        <main  className="main">
+        <h2>CEP : {cep.cep}</h2>
+
+        <span>Rua: {cep.logradouro}</span>
+        <span>Complemento : {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep.localidade} - Estado: {cep.uf}</span>
+        <span>DDD do local: {cep.ddd}</span>
+
       </main>
+      )}
+      </div>
     </div>
   );
 }
